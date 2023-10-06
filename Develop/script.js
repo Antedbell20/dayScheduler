6eda4b2bb427cca5b9fdce37416872d5f7601e5a
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-10-05 20:28:11 -0500

    localstorage
    gets input and saves it in local storage

--- Develop/script.js
+++ Develop/script.js
@@ -3,4 +3,10 @@ $(document).ready(function () {
     var time = document.querySelector("#currentDay");
     var timeNow = dayjs().format("dddd, MMMM D, YYYY");
     time.textContent = timeNow;
+ //get user input and save in local storage
+$(".saveBtn").on("click", function() {
+    var text = $(this).siblings("description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(text, time);
+  });  
 })
