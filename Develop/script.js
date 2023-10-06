2909944989f47b79601f6aaf8706e3cd2159dc52
Antedbell20 <78629236+Antedbell20@users.noreply.github.com>
2023-10-05 20:29:11 -0500

    background colors
    places the different background colors depending on the current time

--- Develop/script.js
+++ Develop/script.js
@@ -9,4 +9,20 @@ $(".saveBtn").on("click", function() {
     var time = $(this).parent().attr("id");
     localStorage.setItem(text, time);
   });  
+ //sets background colors based off the current time 
+function tracker() {
+    var currentHr = dayjs().hour();
+    $(".time-block").each(function() {
+      var blockHr = parseInt($(this).attr("id").split("-")[1]);
+      if(blockHr < currentHr) {
+        $(this).addClass("past");
+      } else if (blockHr === currentHr) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      }
+    }); 
 })
